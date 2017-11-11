c5d399e325ee2cbdb8025d04384f48a7b4cc79ae
Indra Firmansyah <indransyah@gmail.com>
2017-11-11 10:36:15 +0700

    Add logo

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,9 @@ class Header extends React.Component {
       <div className="container">
         <nav className="navbar navbar-expand-md fixed-top navbar-dark bg-blue-dark">
           <div className="container">
-            <a className="navbar-brand" href="/">Teamy.ID</a>
+            <a className="navbar-brand" href="/">
+              <img src="img/logo.png" width="120px" height="30px"/>
+            </a>
               <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
               </button>
